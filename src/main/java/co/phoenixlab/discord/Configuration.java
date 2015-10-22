abe63f03bf48087078621550ff58aaab389f2ae6
Vincent Zhang <vzhang@purdue.edu>
2015-10-21 20:02:15 -0400

    Add basic config

---
+++ src/main/java/co/phoenixlab/discord/Configuration.java
@@ -0,0 +1,27 @@
+package co.phoenixlab.discord;
+
+public class Configuration {
+
+    private String email;
+    private String password;
+
+    public Configuration() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
