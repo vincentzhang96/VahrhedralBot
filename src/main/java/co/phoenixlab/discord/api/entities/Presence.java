68add24f1dae579a9253238b3f8770fb40ee2090
Vincent Zhang <vzhang@purdue.edu>
2015-10-22 01:39:24 -0400

    Add presences

---
+++ src/main/java/co/phoenixlab/discord/api/entities/Presence.java
@@ -0,0 +1,11 @@
+package co.phoenixlab.discord.api.entities;
+
+public enum Presence {
+    /** User is online and present */
+    ONLINE,
+    /** User is online but away */
+    IDLE,
+    /** User is offline */
+    OFFLINE
+
+}
