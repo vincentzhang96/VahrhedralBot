8962e20bdc1190896b8a869e0ac16ec51b597881
Vincent Zhang <vzhang@purdue.edu>
2015-10-22 10:13:43 -0400

    Add base DiscordApiClient

---
+++ src/main/java/co/phoenixlab/discord/api/DiscordApiClient.java
@@ -0,0 +1,31 @@
+package co.phoenixlab.discord.api;
+
+import java.io.IOException;
+
+public class DiscordApiClient {
+
+    public DiscordApiClient() {
+
+    }
+
+    public void logIn(String email, String password) throws IOException {
+        //  TODO
+    }
+
+    public void sendMessage(String body, String channelId) throws IOException {
+        //  TODO
+    }
+
+    public void deleteMessage(String messageId, String channelId) throws IOException {
+        //  TODO
+    }
+
+    public boolean isOpen() {
+        //  TODO
+        return false;
+    }
+
+
+
+
+}
