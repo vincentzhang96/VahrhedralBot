f7d2a45edefa2797a4a2c471695d87c70ae6ea50
Vincent Zhang <vzhang@purdue.edu>
2015-10-23 19:46:05 -0400

    Implemented logging in

--- src/main/java/co/phoenixlab/discord/api/DiscordApiClient.java
+++ src/main/java/co/phoenixlab/discord/api/DiscordApiClient.java
@@ -1,14 +1,67 @@
 package co.phoenixlab.discord.api;
 
+import co.phoenixlab.discord.VahrhedralBot;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.HttpHeaders;
+import org.apache.http.entity.ContentType;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 public class DiscordApiClient {
+
+    private static final Logger LOGGER = VahrhedralBot.LOGGER;
+
+    private String email;
+    private String password;
+
+    private String token;
 
     public DiscordApiClient() {
 
     }
 
     public void logIn(String email, String password) throws IOException {
+        //  TODO
+        LOGGER.info("Attempting to log in as {}...", email);
+        this.email = email;
+        this.password = password;
+
+        JSONObject auth = new JSONObject();
+        auth.put("email", email);
+        auth.put("password", password);
+
+        HttpResponse<JsonNode> response;
+        try {
+             response = Unirest.post(ApiConst.LOGIN_ENDPOINT).
+                    header(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType()).
+                    body(auth.toJSONString()).
+                    asJson();
+        } catch (UnirestException e) {
+            throw new IOException(e);
+        }
+        int status = response.getStatus();
+
+        if (status != HttpURLConnection.HTTP_OK) {
+            if (status == HttpURLConnection.HTTP_FORBIDDEN) {
+                //  Servers return FORBIDDEN for bad credentials
+                LOGGER.warn("Unable to log in with given credentials: ", response.getStatusText());\
+            } else {
+                LOGGER.warn("Unable to log in, Discord may be having issues");
+            }
+            throw new IOException("Unable to auth: HTTP " + response.getStatus() + ": " + response.getStatusText());
+        }
+        token = response.getBody().getObject().getString("token");
+        LOGGER.info("Successfully logged in, using token {}", token);
+        openWebSocket();
+    }
+
+    private void openWebSocket() {
         //  TODO
     }
 
