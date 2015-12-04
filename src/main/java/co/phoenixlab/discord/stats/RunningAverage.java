e1dab87643a97d8a53e79e3acc0b8fa96d82b094
Vincent Zhang <vzhang@purdue.edu>
2015-12-04 12:26:06 -0500

    Add min max summary to RunningAverage

--- src/main/java/co/phoenixlab/discord/stats/RunningAverage.java
+++ src/main/java/co/phoenixlab/discord/stats/RunningAverage.java
@@ -1,33 +1,47 @@
 package co.phoenixlab.discord.stats;
 
+import static java.lang.Math.*;
+
 public class RunningAverage {
 
     private double running;
     private int count;
+    private double min;
+    private double max;
 
     public RunningAverage() {
         running = 0;
         count = 0;
+        min = Double.MAX_VALUE;
+        max = 0D;
     }
 
     public void add(long val) {
         running = (running * (double) count + (double) val) / (double) (count + 1);
         count++;
+        min = min(min, val);
+        max = max(max, val);
     }
 
     public void add(int val) {
         running = (running * (double) count + (double) val) / (double) (count + 1);
         count++;
+        min = min(min, val);
+        max = max(max, val);
     }
 
     public void add(float val) {
         running = (running * (double) count + (double) val) / (double) (count + 1);
         count++;
+        min = min(min, val);
+        max = max(max, val);
     }
 
     public void add(double val) {
         running = (running * (double) count + (double) val) / (double) (count + 1);
         count++;
+        min = min(min, val);
+        max = max(max, val);
     }
 
     public double getRunningAverage() {
@@ -36,5 +50,17 @@ public class RunningAverage {
 
     public int getSize() {
         return count;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public String summary() {
+        return String.format("%.2f/%.2f/%.2f", min, running, max);
     }
 }
