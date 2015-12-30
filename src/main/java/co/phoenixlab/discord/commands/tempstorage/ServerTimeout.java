7501688315cc1cf4f3bd074fee0764c6ca893d58
Vincent Zhang <vzhang@purdue.edu>
2015-12-30 18:07:49 -0500

    Add structs for timeout info

--- src/main/java/co/phoenixlab/discord/commands/tempstorage/ServerTimeout.java
+++ src/main/java/co/phoenixlab/discord/commands/tempstorage/ServerTimeout.java
@@ -2,6 +2,7 @@ package co.phoenixlab.discord.commands.tempstorage;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.Future;
 
 public class ServerTimeout {
 
@@ -11,12 +12,15 @@ public class ServerTimeout {
     private final Duration duration;
     private final Instant endTime;
     private final String issuedByUserId;
+    private final Future<Void> timerFuture;
 
-    public ServerTimeout(Duration duration, Instant startTime, String userId, String serverId, String issuedByUserId) {
+    public ServerTimeout(Duration duration, Instant startTime, String userId, String serverId, String issuedByUserId,
+                         Future<Void> timerFuture) {
         this.duration = duration;
         this.startTime = startTime;
         this.userId = userId;
         this.serverId = serverId;
+        this.timerFuture = timerFuture;
         this.endTime = startTime.plus(duration);
         this.issuedByUserId = issuedByUserId;
     }
@@ -43,5 +47,9 @@ public class ServerTimeout {
 
     public String getIssuedByUserId() {
         return issuedByUserId;
+    }
+
+    public Future<Void> getTimerFuture() {
+        return timerFuture;
     }
 }
