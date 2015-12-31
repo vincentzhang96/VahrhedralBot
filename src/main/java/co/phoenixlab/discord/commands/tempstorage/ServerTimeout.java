4a3c8685814e91f700d4de2b6be4c55dfe310e38
Vincent Zhang <vzhang@purdue.edu>
2015-12-31 00:44:41 -0500

    Add lastUsername field

--- src/main/java/co/phoenixlab/discord/commands/tempstorage/ServerTimeout.java
+++ src/main/java/co/phoenixlab/discord/commands/tempstorage/ServerTimeout.java
@@ -8,18 +8,20 @@ public class ServerTimeout {
 
     private final String serverId;
     private final String userId;
+    private final String lastUsername;
     private final Instant startTime;
     private final Duration duration;
     private final Instant endTime;
     private final String issuedByUserId;
     private final transient Future<Void> timerFuture;
 
-    public ServerTimeout(Duration duration, Instant startTime, String userId, String serverId, String issuedByUserId,
-                         Future<Void> timerFuture) {
+    public ServerTimeout(Duration duration, Instant startTime, String userId, String serverId, String lastUsername,
+                         String issuedByUserId, Future<Void> timerFuture) {
         this.duration = duration;
         this.startTime = startTime;
         this.userId = userId;
         this.serverId = serverId;
+        this.lastUsername = lastUsername;
         this.timerFuture = timerFuture;
         this.endTime = startTime.plus(duration);
         this.issuedByUserId = issuedByUserId;
@@ -51,5 +53,9 @@ public class ServerTimeout {
 
     public Future<Void> getTimerFuture() {
         return timerFuture;
+    }
+
+    public String getLastUsername() {
+        return lastUsername;
     }
 }
