ed969c4562b8ad2dacd1df486a1ef9f472833aa9
Vincent Zhang <vzhang@purdue.edu>
2016-02-16 11:53:07 -0500

    Add basic Identity for Identifiables

---
+++ src/main/java/co/phoenixlab/discord/api2/Identity.java
@@ -0,0 +1,18 @@
+package co.phoenixlab.discord.api2;
+
+/**
+ * Represents only an ID, for use when dealing with raw IDs and passing to APIs that operate
+ * off of IDs. General usage is discouraged, please use more suitable implementations
+ */
+public class Identity extends AbstractIdentifiable {
+
+    public Identity(String id)
+            throws NullPointerException, IllegalArgumentException {
+        super(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
