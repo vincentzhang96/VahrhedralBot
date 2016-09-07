4b33f066ef946a7940aa76eeeff252b7d90cb7ee
Joseph Morain <aelphaeis@live.com>
2016-09-07 15:57:58 -0400

    Refactored code to reduce duplication

--- src/main/java/co/phoenixlab/discord/commands/DnCommands.java
+++ src/main/java/co/phoenixlab/discord/commands/DnCommands.java
@@ -2,7 +2,6 @@ package co.phoenixlab.discord.commands;
 
 import co.phoenixlab.common.lang.SafeNav;
 import co.phoenixlab.common.lang.number.ParseInt;
-import co.phoenixlab.common.lang.number.ParseLong;
 import co.phoenixlab.common.localization.Localizer;
 import co.phoenixlab.discord.Command;
 import co.phoenixlab.discord.CommandDispatcher;
@@ -13,6 +12,7 @@ import co.phoenixlab.discord.api.DiscordApiClient;
 import co.phoenixlab.discord.commands.dn.CalculateCriticalChance;
 import co.phoenixlab.discord.commands.dn.CalculateCriticalDamage;
 import co.phoenixlab.discord.dntrack.VersionTracker;
+import co.phoenixlab.discord.util.StringUtilities;
 
 import java.util.Map;
 import java.util.StringJoiner;
@@ -156,7 +156,7 @@ public class DnCommands {
                             level, (int) fd, fdPercent * 100D),
                             context.getChannel());
                 } else {
-                    int fd = (int) parseStat(quantity);
+                    int fd = (int) StringUtilities.parseAlphanumeric(quantity, context.getBot().getLocalizer());;
                     int level = 80;
                     if (split.length >= 2) {
                         level = ParseInt.parseOrDefault(split[1], level);
@@ -196,9 +196,9 @@ public class DnCommands {
         String[] split = args.split(" ");
         if (split.length >= 3) {
             try {
-                double rawHp = parseStat(split[0]);
-                double rawDef = parseStat(split[1]);
-                double rawMDef = parseStat(split[2]);
+                double rawHp = StringUtilities.parseAlphanumeric(split[0], context.getBot().getLocalizer());
+                double rawDef = StringUtilities.parseAlphanumeric(split[1], context.getBot().getLocalizer());
+                double rawMDef = StringUtilities.parseAlphanumeric(split[2], context.getBot().getLocalizer());
                 if (rawHp < 0 || rawDef < 0 || rawMDef < 0) {
                     throw new NumberFormatException();
                 }
@@ -239,62 +239,4 @@ public class DnCommands {
         defPercent = Math.max(0, Math.min(DEFENSE_MAX_PERCENT, defPercent)) * 100D;
         return defPercent;
     }
-
-
-    private double parseStat(String s) throws NumberFormatException {
-        //  Check if it ends in thousand or million
-        String thousandSuffix = loc.localize("commands.dn.defense.suffix.thousand");
-        int start = s.indexOf(thousandSuffix);
-        double working;
-        double ret = 0;
-        if (start == 0) {
-            //  Invalid, cannot be just "k"
-            throw new NumberFormatException();
-        }
-        if (start != -1) {
-            String num = s.substring(0, start);
-            try {
-                working = Double.parseDouble(num);
-            } catch (NumberFormatException nfe) {
-                throw new NumberFormatException();
-            }
-            ret = working * 1000.0;
-        } else {
-            String millionSuffix = loc.localize("commands.dn.defense.suffix.million");
-            start = s.indexOf(millionSuffix);
-            if (start == 0) {
-                //  Invalid, cannot be just "m"
-                throw new NumberFormatException();
-            }
-            if (start != -1) {
-                String num = s.substring(0, start);
-                try {
-                    working = Double.parseDouble(num);
-                } catch (NumberFormatException nfe) {
-                    throw new NumberFormatException();
-                }
-                ret = working * 1000000.0;
-            } else {
-                String billionSuffix = loc.localize("commands.dn.defense.suffix.billion");
-                start = s.indexOf(billionSuffix);
-                if (start == 0) {
-                    //  Invalid, cannot be just "b"
-                    throw new NumberFormatException();
-                }
-                if (start != -1) {
-                    String num = s.substring(0, start);
-                    try {
-                        working = Double.parseDouble(num);
-                    } catch (NumberFormatException nfe) {
-                        throw new NumberFormatException();
-                    }
-                    ret = working * 1000000000.0;
-                } else {
-                    ret = ParseLong.parseDec(s);
-                }
-            }
-        }
-        return ret;
-    }
-
 }
