baf34695cc19e5f197acae7152ed6904b0c482ab
Joseph Morain <aelphaeis@live.com>
2016-09-07 12:48:43 -0400

    Added parseAlphanumeric to StringUtilities

--- src/main/java/co/phoenixlab/discord/util/StringUtilities.java
+++ src/main/java/co/phoenixlab/discord/util/StringUtilities.java
@@ -3,6 +3,7 @@ package co.phoenixlab.discord.util;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import co.phoenixlab.common.lang.number.ParseLong;
 import co.phoenixlab.common.localization.LocaleStringProvider;
 import co.phoenixlab.common.localization.Localizer;
 import co.phoenixlab.common.localization.LocalizerImpl;
@@ -35,4 +36,67 @@ public class StringUtilities {
         localizer.addLocaleStringProvider(provider);
         return localizer;
 	}
+
+	public static double parseAlphanumeric(String value, Localizer localizer) {
+	//  Check if it ends in thousand or million
+        String thousandSuffix = localizer.localize("commands.dn.defense.suffix.thousand");
+        int start = value.indexOf(thousandSuffix);
+        double working;
+        double ret = 0;
+        if (start == 0) {
+            //  Invalid, cannot be just "k"
+            throw new NumberFormatException();
+        }
+        if (start != -1) {
+            String num = value.substring(0, start);
+            try {
+                working = Double.parseDouble(num);
+            } 
+            catch (NumberFormatException nfe) {
+                throw new NumberFormatException();
+            }
+            ret = working * 1000.0;
+        }
+        else {
+            String millionSuffix = localizer.localize("commands.dn.defense.suffix.million");
+            start = value.indexOf(millionSuffix);
+            if (start == 0) {
+                //  Invalid, cannot be just "m"
+                throw new NumberFormatException();
+            }
+            if (start != -1) {
+                String num = value.substring(0, start);
+                try {
+                    working = Double.parseDouble(num);
+                } 
+                catch (NumberFormatException nfe) {
+                    throw new NumberFormatException();
+                }
+                ret = working * 1000000.0;
+            }
+            else {
+                String billionSuffix = localizer.localize("commands.dn.defense.suffix.billion");
+                start = value.indexOf(billionSuffix);
+                if (start == 0) {
+                    //  Invalid, cannot be just "b"
+                    throw new NumberFormatException();
+                }
+                if (start != -1) {
+                    String num = value.substring(0, start);
+                    try {
+                        working = Double.parseDouble(num);
+                    }
+                    catch (NumberFormatException nfe) {
+                        throw new NumberFormatException();
+                    }
+                    ret = working * 1000000000.0;
+                } 
+                else {
+                    ret = ParseLong.parseDec(value);
+                }
+            }
+        }
+        return ret;
+	}
+	
 }
