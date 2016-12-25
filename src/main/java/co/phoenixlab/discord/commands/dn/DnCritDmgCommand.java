c20c19c60aac138c4460744fc049ab755c965b97
Vincent Zhang <vzhang@purdue.edu>
2016-12-24 20:41:35 -0500

    Fix incorrect minimum

--- src/main/java/co/phoenixlab/discord/commands/dn/DnCritDmgCommand.java
+++ src/main/java/co/phoenixlab/discord/commands/dn/DnCritDmgCommand.java
@@ -164,7 +164,7 @@ public class DnCritDmgCommand implements Command {
         if (percent < 0) {
             throw new IllegalArgumentException(loc.localize(
                     "commands.dn.critdmg.response.critdmg_out_of_range",
-                    0, (int) (CRITDMG_MAX_PERCENT * 100D + 200F)
+                    0, 200
             ));
         }
         percent = Math.min(percent, CRITDMG_MAX_PERCENT);
