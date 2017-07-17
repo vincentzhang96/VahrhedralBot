21e219b0e578e44daf05f068c301fb6347ee90cb
Vincent Zhang <vzhang@purdue.edu>
2017-07-17 10:17:20 -0700

    Redo internal rep of permissions

--- src/main/java/co/phoenixlab/discord/api/entities/Permission.java
+++ src/main/java/co/phoenixlab/discord/api/entities/Permission.java
@@ -3,43 +3,43 @@ package co.phoenixlab.discord.api.entities;
 import java.util.EnumSet;
 
 public enum Permission {
-    GEN_CREATE_INSTANT_INVITE(0),
-    GEN_KICK_MEMBERS(1),
-    GEN_BAN_MEMBERS(2),
-    ADMINISTRATOR(4),
-    GEN_MANAGE_CHANNELS(5),
-    GEN_MANAGE_SERVER(6),
-    CHAT_READ_MESSAGES(10),
-    CHAT_SEND_MESSAGES(11),
-    CHAT_SEND_TTS_MESSAGES(12),
-    CHAT_MANAGE_MESSAGES(13),
-    CHAT_EMBED_LINKS(14),
-    CHAT_ATTACH_FILES(15),
-    CHAT_READ_MESSAGE_HISTORY(16),
-    CHAT_MENTION_EVERYONE(17),
-    VOICE_CONNECT(20),
-    VOICE_SPEAK(21),
-    VOICE_MUTE_MEMBERS(22),
-    VOICE_DEAFEN_MEMBERS(23),
-    VOICE_MOVE_MEMBERS(24),
-    VOICE_USE_VAD(25),
-    NICK_CHANGE_NICKNAME(26),
-    NICK_MANAGE_NICKNAMES(27),
-    GEN_MANAGE_ROLES(28);
+    GEN_CREATE_INSTANT_INVITE(0x00000001),
+    GEN_KICK_MEMBERS(0x00000002),
+    GEN_BAN_MEMBERS(0x00000004),
+    ADMINISTRATOR(0x00000008),
+    GEN_MANAGE_CHANNELS(0x00000010),
+    GEN_MANAGE_SERVER(0x00000020),
+    CHAT_READ_MESSAGES(0x00000400),
+    CHAT_SEND_MESSAGES(0x00000800),
+    CHAT_SEND_TTS_MESSAGES(0x00001000),
+    CHAT_MANAGE_MESSAGES(0x00002000),
+    CHAT_EMBED_LINKS(0x00004000),
+    CHAT_ATTACH_FILES(0x00008000),
+    CHAT_READ_MESSAGE_HISTORY(0x00010000),
+    CHAT_MENTION_EVERYONE(0x00020000),
+    VOICE_CONNECT(0x00100000),
+    VOICE_SPEAK(0x00200000),
+    VOICE_MUTE_MEMBERS(0x00400000),
+    VOICE_DEAFEN_MEMBERS(0x00800000),
+    VOICE_MOVE_MEMBERS(0x01000000),
+    VOICE_USE_VAD(0x02000000),
+    NICK_CHANGE_NICKNAME(0x04000000),
+    NICK_MANAGE_NICKNAMES(0x08000000),
+    GEN_MANAGE_ROLES(0x10000000);
 
 
-    private final int bitNum;
+    private final int mask;
 
-    Permission(int bitNum) {
-        this.bitNum = bitNum;
+    Permission(int mask) {
+        this.mask = mask;
     }
 
-    public int getBitNum() {
-        return bitNum;
+    public int getMask() {
+        return mask;
     }
 
     public long toLong() {
-        return 1L << bitNum;
+        return (long) mask;
     }
 
     public boolean test(long l) {
